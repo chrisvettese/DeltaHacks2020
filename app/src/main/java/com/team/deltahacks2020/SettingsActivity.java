c44aa4a3b041074e89660dfc1efb8738ce804432
angusryoung <angusryoung@gmail.com>
2020-01-25 14:04:18 -0500

    working on getting key from internal storage

--- app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
+++ app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
@@ -4,6 +4,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SettingsActivity extends AppCompatActivity {
     private static AsyncTask<String, Integer, String> task;
 
@@ -13,12 +18,35 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         new MyAsyncTask().execute();
+
+
     }
 
     private static class MyAsyncTask extends AsyncTask<String, Integer, String> {
         @Override
         protected String doInBackground(String... strings) {
             //Angus put your code here
+            File file = new File("settings.txt");
+            if (file.exists()){
+                try (FileReader reader = new FileReader(file)){
+
+
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else{
+                //Phone is camera
+
+            }
+
+
+
+
+
             return null;
         }
     }

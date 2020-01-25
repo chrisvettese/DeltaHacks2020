19699e20fd3d79be34442bb58d681f64064cd653
Chris Vettese <cvett018@uottawa.ca>
2020-01-25 13:16:41 -0500

    Async method for settings activity

--- app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
+++ app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
@@ -1,13 +1,25 @@
 package com.team.deltahacks2020;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+    private static AsyncTask<String, Integer, String> task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        new MyAsyncTask().execute();
+    }
+
+    private static class MyAsyncTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            //Angus put your code here
+            return null;
+        }
     }
 }

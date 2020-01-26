1443144917fefebf1a4f135d2effdf7eb1e9bcc1
Melissa Wu <mwu070@uottawa.ca>
2020-01-25 19:52:59 -0500

    functions a bit more now

--- app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
+++ app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
@@ -1,13 +1,57 @@
 package com.team.deltahacks2020;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    TextView name;
+    GoogleSignInClient mGoogleSignInClient;
+    Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        name = findViewById(R.id.TV1);
+        signOut = findViewById(R.id.btnSignOut);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnSignOut:
+                        signOut();
+                        break;
+                }
+            }
+        });
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            name.setText(personName);
+        }
+
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(SettingsActivity.this, "Signed out", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
+

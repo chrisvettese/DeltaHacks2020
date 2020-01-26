59e9071f6b02de1c53f559d8d1a45caba19c6370
Melissa Wu <mwu070@uottawa.ca>
2020-01-25 22:56:57 -0500

    sign out button works ish

--- app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
+++ app/src/main/java/com/team/deltahacks2020/SettingsActivity.java
@@ -12,10 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,6 +30,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import javax.annotation.Nonnull;
 import java.io.*;
+import java.text.BreakIterator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +41,8 @@ public class SettingsActivity extends AppCompatActivity {
     String userID;
     Button userButt;
     Button camButt;
+    GoogleSignInOptions gso;
+    TextView statusTextView;
 
 
     GoogleSignInClient mGoogleSignInClient;
@@ -90,6 +97,11 @@ public class SettingsActivity extends AppCompatActivity {
                         }
                     }*/
                 });
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
 
@@ -182,13 +194,19 @@ public class SettingsActivity extends AppCompatActivity {
 
 
 
+//    private void signOut() {
+//        mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                Toast.makeText(SettingsActivity.this, "Signed out", Toast.LENGTH_SHORT);
+//            }
+//        });
+//    }
+    
     private void signOut() {
-        mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(SettingsActivity.this, "Signed out", Toast.LENGTH_SHORT);
-            }
-        });
+        FirebaseAuth.getInstance().signOut();
+        finish();
+
     }
 }
 

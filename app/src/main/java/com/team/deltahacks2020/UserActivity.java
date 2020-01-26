6fbb6e6c9f5fd3466999f0124927b443e4a23c1a
Chris Vettese <cvett018@uottawa.ca>
2020-01-26 02:09:12 -0500

    Messaging test

--- app/src/main/java/com/team/deltahacks2020/UserActivity.java
+++ app/src/main/java/com/team/deltahacks2020/UserActivity.java
@@ -1,7 +1,14 @@
 package com.team.deltahacks2020;
 
 import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.*;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        final DocumentReference docRef = db.collection("controller").document(email);
+        docRef.addSnapshotListener((snapshot, e) -> {
+            if (e != null) {
+                System.out.println("Listen failed");
+                return;
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                System.out.println("TEST " + snapshot.getData());
+            } else {
+                System.out.println("TEST : null");
+            }
+        });
     }
 }

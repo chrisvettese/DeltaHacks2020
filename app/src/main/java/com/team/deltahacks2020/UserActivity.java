1499f0fe929959a86456616fe268f0e46ebb06cb
Melissa Wu <mwu070@uottawa.ca>
2020-01-26 02:50:35 -0500

    sign out button works ish

--- app/src/main/java/com/team/deltahacks2020/UserActivity.java
+++ app/src/main/java/com/team/deltahacks2020/UserActivity.java
@@ -1,13 +1,24 @@
 package com.team.deltahacks2020;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class UserActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
     }
+
 }
